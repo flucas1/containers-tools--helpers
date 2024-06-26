bde74b61dad4de0c5f1ba4d7e7c04454e247cb21
Fernando Lucas <info@fernandolucas.info>
2024-06-26 22:19:44 +0200

    update

--- wine-dotnet-dummyapp.sh
+++ wine-dotnet-dummyapp.sh
@@ -7,4 +7,4 @@ WINEATOMIC="/wine-atomic.sh"
 HELPERSPATH="/helpers"
 
 LOCALDOTNETDUMMYAPP="${HELPERSPATH}/dotnet-dummyapp.py"
-#$WINEATOMIC python.exe "$(winepath ${LOCALDOTNETDUMMYAPP})"
+$WINEATOMIC python.exe "$(winepath ${LOCALDOTNETDUMMYAPP})"
