874602783179ac31fc78d94123f20e5371d1225b
Fernando Lucas <info@fernandolucas.info>
2024-09-21 18:23:18 +0200

    update

--- setup-dotnetextras.sh
+++ setup-dotnetextras.sh
@@ -6,6 +6,7 @@ set -x
 HELPERSPATH="/helpers"
 HELPERSCACHE="/helperscache"
 
-/opt/dotnet/dotnet workload install wasm-experimental wasm-tools
+/opt/dotnet/dotnet workload install wasm-experimental
+/opt/dotnet/dotnet workload install wasm-tools
 
 /opt/dotnet/dotnet new install avalonia.templates
--- wine-dotnetextras.sh
+++ wine-dotnetextras.sh
@@ -5,6 +5,7 @@ set -x
 
 WINEATOMIC="/wine-atomic.sh"
 
-$WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" workload install wasm-experimental wasm-tools
+$WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" workload install wasm-experimental
+$WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" workload install wasm-tools
 
 $WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" new install avalonia.templates
