4e7870d16cb7de035b844c9c4d9e0770cac2c2f8
Fernando Lucas <info@fernandolucas.info>
2024-10-28 12:46:31 +0100

    msttcorefonts/accepted-mscorefonts-eula

--- setup-fonts-microsoft.sh
+++ setup-fonts-microsoft.sh
@@ -5,6 +5,7 @@ set -x
 
 HELPERSPATH="/helpers"
 
+echo "ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true" | debconf-set-selections
 echo "ttf-mscorefonts-installer msttcorefonts/dldir string /root/mscorefonts" | debconf-set-selections
 ${HELPERSPATH}/apt-retry-install.sh ttf-mscorefonts-installer
 echo "ttf-mscorefonts-installer msttcorefonts/dldir string" | debconf-set-selections
