560fdc080296aab6555e56d047d14e466280854b
Fernando Lucas <info@fernandolucas.info>
2024-11-14 23:05:53 +0100

    update

--- setup-dotnetsdk.sh
+++ setup-dotnetsdk.sh
@@ -24,7 +24,12 @@ install_dotnetsdk()
   if [ ! -f "${LOCALCACHEFILENAME}" ] ; then
     rm -f "${LOCALCACHEFILENAME}"
     mkdir -p "${HELPERSCACHE}"
-    MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0 ; while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do echo "Retry #$COUNTER" ; if timeout 900s wget -4 --no-verbose --retry-connrefused --waitretry=3 --tries=20 "${DOWNLOADURL}" -O "${LOCALCACHEFILENAME}" ; then SUCCESS=1 ; else COUNTER=$(( $COUNTER + 1 )) ; sleep 5s ; fi ; done ; [ $SUCCESS -eq 1 ]
+    MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
+    while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
+      echo "Retry #$COUNTER"
+      if timeout 900s wget -4 --no-verbose --retry-connrefused --waitretry=3 --tries=20 "${DOWNLOADURL}" -O "${LOCALCACHEFILENAME}" ; then SUCCESS=1 ; else COUNTER=$(( $COUNTER + 1 )) ; sleep 5s ; fi
+    done
+    [ $SUCCESS -eq 1 ]
   fi
   [ -f "${LOCALCACHEFILENAME}" ]
 
@@ -49,9 +54,11 @@ getversion_dotnetsdk()
   PARTARCH="$1"
   LINENUMBER="$2"
 
-  MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
+  MAXRETRIES=30
+  COUNTER=0
+  SUCCESS=0
   while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
-    echo "Retry #$COUNTER"
+    #echo "Retry #$COUNTER"
     DOTNETSDKVERSION="$(timeout 900s wget --quiet --no-verbose --retry-connrefused --waitretry=3 --tries=20 https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/releases-index.json -O - | jq -r '.["releases-index"][] | select(."support-phase"=="active") | ."latest-sdk"' | sort --version-sort --reverse | awk \"NR==$LINENUMBER\")"
     if [ "${DOTNETSDKVERSION}" != "" ] ; then SUCCESS=1 ; else COUNTER=$(( $COUNTER + 1 )) ; sleep 5s ; fi
   done
