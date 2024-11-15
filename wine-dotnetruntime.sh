861fc3e218409cb80e7545cbe980e6f7675d8ec3
Fernando Lucas <info@fernandolucas.info>
2024-11-15 16:16:47 +0100

    another step

--- wine-dotnetruntime.sh
+++ wine-dotnetruntime.sh
@@ -9,6 +9,39 @@ DIRECTINSTALL="$1"
 
 install_dotnetruntime()
 {
+  # follow "channel-version" and "releases.json" from previous json into https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/8.0/releases.json and select right link
+  FILENAME="dotnet-runtime-${DOTNETRUNTIMEVERSION}-win-${PARTARCH}.exe"
+  DOWNLOADURL="https://dotnetcli.blob.core.windows.net/dotnet/Runtime/${DOTNETRUNTIMEVERSION}/${FILENAME}"
+  LOCALCACHEFILENAME="${HELPERSCACHE}/${FILENAME}"
+  if [ ! -f "${LOCALCACHEFILENAME}" ] ; then
+    rm -f "${LOCALCACHEFILENAME}"
+    mkdir -p "${HELPERSCACHE}"
+    MAXRETRIES=30
+    COUNTER=0
+    SUCCESS=0
+    while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do echo "Retry #$COUNTER" ; if timeout 900s wget -4 --no-verbose --retry-connrefused --waitretry=3 --tries=20 "${DOWNLOADURL}" -O "${LOCALCACHEFILENAME}" ; then SUCCESS=1 ; else COUNTER=$(( $COUNTER + 1 )) ; sleep 5s ; fi ; done
+    [ $SUCCESS -eq 1 ]
+  fi
+  [ -f "${LOCALCACHEFILENAME}" ]
+  $WINEATOMIC "$(winepath ${LOCALCACHEFILENAME})" /install /quiet /norestart
+
+  # follow "channel-version" and "releases.json" from previous json into https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/8.0/releases.json and select right link
+  FILENAME="windowsdesktop-runtime-${DOTNETRUNTIMEVERSION}-win-${PARTARCH}.exe"
+  DOWNLOADURL="https://dotnetcli.blob.core.windows.net/dotnet/Sdk/${DOTNETRUNTIMEVERSION}/${FILENAME}"
+  LOCALCACHEFILENAME="${HELPERSCACHE}/${FILENAME}"
+  if [ ! -f "${LOCALCACHEFILENAME}" ] ; then
+    rm -f "${LOCALCACHEFILENAME}"
+    mkdir -p "${HELPERSCACHE}"
+    MAXRETRIES=30
+    COUNTER=0
+    SUCCESS=0
+    while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do echo "Retry #$COUNTER" ; if timeout 900s wget -4 --no-verbose --retry-connrefused --waitretry=3 --tries=20 "${DOWNLOADURL}" -O "${LOCALCACHEFILENAME}" ; then SUCCESS=1 ; else COUNTER=$(( $COUNTER + 1 )) ; sleep 5s ; fi ; done
+    [ $SUCCESS -eq 1 ]
+  fi
+  [ -f "${LOCALCACHEFILENAME}" ]
+  $WINEATOMIC "$(winepath ${LOCALCACHEFILENAME})" /install /quiet /norestart
+
+  #$WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" --info
 }
 
 getversion_dotnetruntime()
@@ -17,7 +50,9 @@ getversion_dotnetruntime()
   COUNTER=0
   SUCCESS=0
   while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
-    echo "Retry #$COUNTER" ; DOTNETRUNTIMEVERSION="$(timeout 900s wget --quiet --no-verbose --retry-connrefused --waitretry=3 --tries=20 https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/releases-index.json -O - | jq -r '.["releases-index"][] | select(."support-phase"=="active") | ."latest-runtime"' | sort --version-sort --reverse | head -n 1)" ; if [ "${DOTNETRUNTIMEVERSION}" != "" ] ; then SUCCESS=1 ; else COUNTER=$(( $COUNTER + 1 )) ; sleep 5s ; fi
+    echo "Retry #$COUNTER"
+    DOTNETRUNTIMEVERSION="$(timeout 900s wget --quiet --no-verbose --retry-connrefused --waitretry=3 --tries=20 https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/releases-index.json -O - | jq -r '.["releases-index"][] | select(."support-phase"=="active") | ."latest-runtime"' | sort --version-sort --reverse | head -n 1)"
+    if [ "${DOTNETRUNTIMEVERSION}" != "" ] ; then SUCCESS=1 ; else COUNTER=$(( $COUNTER + 1 )) ; sleep 5s ; fi
   done
   [ $SUCCESS -eq 1 ]
   [ "${DOTNETRUNTIMEVERSION}" != "" ]
@@ -28,36 +63,6 @@ if [ "${ARCHITECTURE}" = "amd64" ] ; then PARTARCH="x64" ; else if [ "${ARCHITEC
 [ "${PARTARCH}" != "" ]
 
 DOTNETRUNTIMEVERSION="$(getversion_dotnetruntime ${PARTARCH} 1)"
+install_dotnetruntime "${PARTARCH}" "${DOTNETRUNTIMEVERSION}"
 
-# follow "channel-version" and "releases.json" from previous json into https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/8.0/releases.json and select right link
-FILENAME="dotnet-runtime-${DOTNETRUNTIMEVERSION}-win-${PARTARCH}.exe"
-DOWNLOADURL="https://dotnetcli.blob.core.windows.net/dotnet/Runtime/${DOTNETRUNTIMEVERSION}/${FILENAME}"
-LOCALCACHEFILENAME="${HELPERSCACHE}/${FILENAME}"
-if [ ! -f "${LOCALCACHEFILENAME}" ] ; then
-  rm -f "${LOCALCACHEFILENAME}"
-  mkdir -p "${HELPERSCACHE}"
-  MAXRETRIES=30
-  COUNTER=0
-  SUCCESS=0
-  while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do echo "Retry #$COUNTER" ; if timeout 900s wget -4 --no-verbose --retry-connrefused --waitretry=3 --tries=20 "${DOWNLOADURL}" -O "${LOCALCACHEFILENAME}" ; then SUCCESS=1 ; else COUNTER=$(( $COUNTER + 1 )) ; sleep 5s ; fi ; done
-  [ $SUCCESS -eq 1 ]
-fi
-[ -f "${LOCALCACHEFILENAME}" ]
-$WINEATOMIC "$(winepath ${LOCALCACHEFILENAME})" /install /quiet /norestart
 
-# follow "channel-version" and "releases.json" from previous json into https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/8.0/releases.json and select right link
-FILENAME="windowsdesktop-runtime-${DOTNETRUNTIMEVERSION}-win-${PARTARCH}.exe"
-DOWNLOADURL="https://dotnetcli.blob.core.windows.net/dotnet/Sdk/${DOTNETRUNTIMEVERSION}/${FILENAME}"
-LOCALCACHEFILENAME="${HELPERSCACHE}/${FILENAME}"
-if [ ! -f "${LOCALCACHEFILENAME}" ] ; then
-  rm -f "${LOCALCACHEFILENAME}"
-  mkdir -p "${HELPERSCACHE}"
-  MAXRETRIES=30
-  COUNTER=0
-  SUCCESS=0
-  while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do echo "Retry #$COUNTER" ; if timeout 900s wget -4 --no-verbose --retry-connrefused --waitretry=3 --tries=20 "${DOWNLOADURL}" -O "${LOCALCACHEFILENAME}" ; then SUCCESS=1 ; else COUNTER=$(( $COUNTER + 1 )) ; sleep 5s ; fi ; done
-  [ $SUCCESS -eq 1 ]
-fi[ -f "${LOCALCACHEFILENAME}" ]
-$WINEATOMIC "$(winepath ${LOCALCACHEFILENAME})" /install /quiet /norestart
-
-#$WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" --info
--- wine-dotnetsdk.sh
+++ wine-dotnetsdk.sh
@@ -9,6 +9,27 @@ DIRECTINSTALL="$1"
 
 install_dotnetsdk()
 {
+  # follow "channel-version" and "releases.json" from previous json into https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/8.0/releases.json and select right link
+  FILENAME="dotnet-sdk-${DOTNETSDKVERSION}-win-${PARTARCH}.exe"
+  DOWNLOADURL="https://dotnetcli.blob.core.windows.net/dotnet/Sdk/${DOTNETSDKVERSION}/${FILENAME}"
+  LOCALCACHEFILENAME="${HELPERSCACHE}/${FILENAME}"
+  if [ ! -f "${LOCALCACHEFILENAME}" ] ; then
+    rm -f "${LOCALCACHEFILENAME}"
+    mkdir -p "${HELPERSCACHE}"
+    MAXRETRIES=30
+    COUNTER=0
+    SUCCESS=0
+    while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do echo "Retry #$COUNTER" ; if timeout 900s wget -4 --no-verbose --retry-connrefused --waitretry=3 --tries=20 "${DOWNLOADURL}" -O "${LOCALCACHEFILENAME}" ; then SUCCESS=1 ; else COUNTER=$(( $COUNTER + 1 )) ; sleep 5s ; fi ; done
+    [ $SUCCESS -eq 1 ]
+  fi
+  [ -f "${LOCALCACHEFILENAME}" ]
+  $WINEATOMIC "$(winepath ${LOCALCACHEFILENAME})" /install /quiet /norestart
+
+  #$WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" --info
+
+  LISTSDK=$($WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" --list-sdks)
+  echo "${LISTSDK}"
+  [ "${LISTSDK}" != "" ]
 }
 
 getversion_dotnetsdk()
@@ -17,7 +38,9 @@ getversion_dotnetsdk()
   COUNTER=0
   SUCCESS=0
   while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
-    echo "Retry #$COUNTER" ; DOTNETSDKVERSION="$(timeout 900s wget --quiet --no-verbose --retry-connrefused --waitretry=3 --tries=20 https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/releases-index.json -O - | jq -r '.["releases-index"][] | select(."support-phase"=="active") | ."latest-sdk"' | sort --version-sort --reverse | head -n 1)" ; if [ "${DOTNETSDKVERSION}" != "" ] ; then SUCCESS=1 ; else COUNTER=$(( $COUNTER + 1 )) ; sleep 5s ; fi
+    echo "Retry #$COUNTER"
+    DOTNETSDKVERSION="$(timeout 900s wget --quiet --no-verbose --retry-connrefused --waitretry=3 --tries=20 https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/releases-index.json -O - | jq -r '.["releases-index"][] | select(."support-phase"=="active") | ."latest-sdk"' | sort --version-sort --reverse | head -n 1)"
+    if [ "${DOTNETSDKVERSION}" != "" ] ; then SUCCESS=1 ; else COUNTER=$(( $COUNTER + 1 )) ; sleep 5s ; fi
   done
   [ $SUCCESS -eq 1 ]
   [ "${DOTNETSDKVERSION}" != "" ]
@@ -28,27 +51,6 @@ if [ "${ARCHITECTURE}" = "amd64" ] ; then PARTARCH="x64" ; else if [ "${ARCHITEC
 [ "${PARTARCH}" != "" ]
 
 DOTNETSDKVERSION="$(getversion_dotnetsdk ${PARTARCH} 1)"
-
-# follow "channel-version" and "releases.json" from previous json into https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/8.0/releases.json and select right link
-FILENAME="dotnet-sdk-${DOTNETSDKVERSION}-win-${PARTARCH}.exe"
-DOWNLOADURL="https://dotnetcli.blob.core.windows.net/dotnet/Sdk/${DOTNETSDKVERSION}/${FILENAME}"
-LOCALCACHEFILENAME="${HELPERSCACHE}/${FILENAME}"
-if [ ! -f "${LOCALCACHEFILENAME}" ] ; then
-  rm -f "${LOCALCACHEFILENAME}"
-  mkdir -p "${HELPERSCACHE}"
-  MAXRETRIES=30
-  COUNTER=0
-  SUCCESS=0
-  while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do echo "Retry #$COUNTER" ; if timeout 900s wget -4 --no-verbose --retry-connrefused --waitretry=3 --tries=20 "${DOWNLOADURL}" -O "${LOCALCACHEFILENAME}" ; then SUCCESS=1 ; else COUNTER=$(( $COUNTER + 1 )) ; sleep 5s ; fi ; done
-  [ $SUCCESS -eq 1 ]
-fi
-[ -f "${LOCALCACHEFILENAME}" ]
-$WINEATOMIC "$(winepath ${LOCALCACHEFILENAME})" /install /quiet /norestart
-
-#$WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" --info
-
-LISTSDK=$($WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" --list-sdks)
-echo "${LISTSDK}"
-[ "${LISTSDK}" != "" ]
+install_dotnetsdk "${PARTARCH}" "${DOTNETSDKVERSION}"
 
 #$WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" tool install --global dotnet-outdated-tool
