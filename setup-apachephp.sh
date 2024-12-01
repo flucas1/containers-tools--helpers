2fe945f3537cd8fe2e0f03ed2a111374fc2aa78a
Fernando Lucas <info@fernandolucas.info>
2024-12-01 15:03:19 +0100

    update

--- setup-apachephp.sh
+++ setup-apachephp.sh
@@ -8,4 +8,8 @@ HELPERSPATH="/helpers"
 ${HELPERSPATH}/apt-retry-install.sh php-fpm
 /usr/sbin/a2enmod proxy_fcgi
 /usr/sbin/a2enmod setenvif
+
+PHPCONF=$(for item in $(ls /etc/apache2/conf-available/php*-fpm.conf) ; do basename $item ; done | sort --version-sort | tail -n 1)
+[ "$PHPCONF" != "" ]
+/usr/sbin/a2enmod $(echo $PHPCONF | rev | cut -c6- | rev)
 /usr/sbin/a2enconf php8.2-fpm
