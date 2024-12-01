d773d45b15fdb99aabd03612b170fac4c90b098e
Fernando Lucas <info@fernandolucas.info>
2024-12-01 15:37:50 +0100

    update

--- setup-apachephp.sh
+++ setup-apachephp.sh
@@ -5,10 +5,15 @@ set -x
 
 HELPERSPATH="/helpers"
 
-${HELPERSPATH}/apt-retry-install.sh php-fpm
-/usr/sbin/a2enmod proxy_fcgi
-/usr/sbin/a2enmod setenvif
+#${HELPERSPATH}/apt-retry-install.sh php-fpm
+#/usr/sbin/a2enmod proxy_fcgi
+#/usr/sbin/a2enmod setenvif
+#PHPCONF=$(for item in $(ls /etc/apache2/conf-available/php*-fpm.conf) ; do basename $item ; done | sort --version-sort | tail -n 1)
+#[ "$PHPCONF" != "" ]
+#/usr/sbin/a2enconf $(echo $PHPCONF | rev | cut -c6- | rev)
 
-PHPCONF=$(for item in $(ls /etc/apache2/conf-available/php*-fpm.conf) ; do basename $item ; done | sort --version-sort | tail -n 1)
-[ "$PHPCONF" != "" ]
-/usr/sbin/a2enconf $(echo $PHPCONF | rev | cut -c6- | rev)
+${HELPERSPATH}/apt-retry-install.sh php
+/usr/bin/php -v
+PHPMODULE=$(for item in $(ls /etc/apache2/mods-available/php*.load) ; do basename $item ; done | sort --version-sort | tail -n 1)
+[ "$PHPMODULE" != "" ]
+/usr/sbin/a2enmod $(echo $PHPMODULE | rev | cut -c6- | rev)
