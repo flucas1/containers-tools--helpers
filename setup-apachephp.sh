55bddd7bc36cc8ed61a3905204163f3b11214510
Fernando Lucas <info@fernandolucas.info>
2024-12-01 12:17:28 +0100

    a2enmod

--- setup-apachephp.sh
+++ setup-apachephp.sh
@@ -6,6 +6,6 @@ set -x
 HELPERSPATH="/helpers"
 
 ${HELPERSPATH}/apt-retry-install.sh php-fpm
-/usr/sbin/setenvif proxy_fcgi
-/usr/sbin/setenvif setenvif
+/usr/sbin/a2enmod proxy_fcgi
+/usr/sbin/a2enmod setenvif
 /usr/sbin/a2enconf php8.2-fpm
