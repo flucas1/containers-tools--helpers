2694b34dc28cfd7c7be2a53ea2eda3f13b0ebf39
Fernando Lucas <info@fernandolucas.info>
2024-12-01 12:08:00 +0100

    setenvif

--- setup-apachephp.sh
+++ setup-apachephp.sh
@@ -6,4 +6,6 @@ set -x
 HELPERSPATH="/helpers"
 
 ${HELPERSPATH}/apt-retry-install.sh php-fpm
-/usr/sbin/a2enmod php8.2-fpm
+/usr/sbin/setenvif proxy_fcgi
+/usr/sbin/setenvif setenvif
+/usr/sbin/a2enconf php8.2-fpm
