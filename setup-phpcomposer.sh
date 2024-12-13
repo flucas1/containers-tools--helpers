02c114fc31eb5b0f026777fb3c576837e825d9a0
Fernando Lucas <info@fernandolucas.info>
2024-12-13 15:19:06 +0100

    TEMPINSTALLFILE

--- setup-phpcomposer.sh
+++ setup-phpcomposer.sh
@@ -3,7 +3,8 @@
 set -e
 set -x
 
-curl -sS https://getcomposer.org/installer -o /root/composer-setup.php
-php /root/composer-setup.php --install-dir=/usr/local/bin --filename=composer
-rm -f /root/composer-setup.php
+TEMPINSTALLFILE=$(mktemp)
+curl -sS https://getcomposer.org/installer -o "$TEMPINSTALLFILE"
+php "$TEMPINSTALLFILE" --install-dir=/usr/local/bin --filename=composer
+rm -f "$TEMPINSTALLFILE"
 /usr/local/bin/composer --version
