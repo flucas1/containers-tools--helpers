88ed735d8e8cee26689c331130f1337dddb6ea30
Fernando Lucas <info@fernandolucas.info>
2024-12-13 15:21:33 +0100

    composer

--- setup-phpcomposer.sh
+++ setup-phpcomposer.sh
@@ -3,8 +3,25 @@
 set -e
 set -x
 
+DOWNLOADURL="https://getcomposer.org/installer"
 TEMPINSTALLFILE=$(mktemp)
-curl -sS https://getcomposer.org/installer -o "$TEMPINSTALLFILE"
+
+MAXRETRIES=30
+COUNTER=0
+SUCCESS=0
+while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
+  echo "Retry #$COUNTER" >&2
+  if timeout 900s wget -4 --no-verbose --retry-connrefused --waitretry=3 --tries=20 "${DOWNLOADURL}" -O "${TEMPINSTALLFILE}" ; then
+    SUCCESS=1
+  else
+    COUNTER=$(( $COUNTER + 1 ))
+    sleep 5s
+  fi
+done
+[ $SUCCESS -eq 1 ]
+
 php "$TEMPINSTALLFILE" --install-dir=/usr/local/bin --filename=composer
+
 rm -f "$TEMPINSTALLFILE"
+
 /usr/local/bin/composer --version
