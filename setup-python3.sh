ddd905d910cb6398d0d70135baea25d534c1bcaa
Fernando Lucas <info@fernandolucas.info>
2024-12-22 02:56:46 +0100

    yod

--- setup-python3.sh
+++ setup-python3.sh
@@ -8,7 +8,6 @@ HELPERSPATH="/helpers"
 ${HELPERSPATH}/apt-retry-install.sh python3-full
 ${HELPERSPATH}/apt-retry-install.sh python3-apt
 ${HELPERSPATH}/apt-retry-install.sh python3-pip
-${HELPERSPATH}/apt-retry-install.sh python3-distutils
 ${HELPERSPATH}/apt-retry-install.sh python3-paramiko
 ${HELPERSPATH}/apt-retry-install.sh python3-requests
 ${HELPERSPATH}/apt-retry-install.sh python3-cryptography
@@ -21,4 +20,5 @@ ${HELPERSPATH}/apt-retry-install.sh python3-mechanize
 ${HELPERSPATH}/apt-retry-install.sh python3-bs4
 ${HELPERSPATH}/apt-retry-install.sh python3-lxml
 ${HELPERSPATH}/apt-retry-install.sh python3-packaging
+#${HELPERSPATH}/apt-retry-install.sh python3-distutils
 ${HELPERSPATH}/apt-retry-install.sh python3-setuptools
