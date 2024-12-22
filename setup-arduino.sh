378733c56f7091ac04afe3972163d159184b2d56
Fernando Lucas <info@fernandolucas.info>
2024-12-22 01:59:42 +0100

    update

--- setup-arduino.sh
+++ setup-arduino.sh
@@ -3,6 +3,12 @@
 set -e
 set -x
 
-HELPERSPATH="/helpers"
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=/opt/arduino sh
 
+#https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_ARM64.tar.gz
+#https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz
 
+#arduino-cli config init
+#arduino-cli core search arduino:avr
+#arduino-cli core install arduino:avr
+#arduino-cli board list
