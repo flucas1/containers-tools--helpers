43347da05ba9d3769ba8610bfe300969cafe1161
Fernando Lucas <info@fernandolucas.info>
2024-12-23 20:49:36 +0100

    upd

--- setup-platformio.sh
+++ setup-platformio.sh
@@ -19,6 +19,32 @@ mkdir -p "${PROJECTDIR}"
 
 platformio project init --project-dir "${PROJECTDIR}"
 cat "${PROJECTDIR}/platformio.ini"
+cat >> "${PROJECTDIR}/platformio.ini" << DELIMITER_END_YAML_FILE
+[platformio]
+#default_envs = esp32dev, nodemcuv2, atmelavr
+
+#[env:esp32dev]
+#platform = espressif32
+#board = esp32dev
+#framework = arduino
+#monitor_speed = 115200
+#upload_speed = 115200
+
+#[env:nodemcuv2]
+#platform = espressif32
+#board = nodemcuv2
+#framework = arduino
+#monitor_speed = 115200
+#upload_speed = 115200
+
+[env:uno]
+platform = atmelavr
+board = uno
+framework = arduino
+monitor_speed = 9600
+upload_speed = 115200
+DELIMITER_END_YAML_FILE
+cat "${PROJECTDIR}/platformio.ini"
 platformio pkg list --project-dir "${PROJECTDIR}"
 
 ln -s /usr/bin/python3 /usr/bin/python
