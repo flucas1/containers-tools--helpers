9e328334e15f2814e793113d9ed71d609adde8c2
Fernando Lucas <info@fernandolucas.info>
2024-12-23 17:15:39 +0100

    update

--- setup-platformio.sh
+++ setup-platformio.sh
@@ -14,17 +14,17 @@ mkdir -p /opt/platformio_shared
 sudo mkdir -p /etc/platformio
 echo "[platformio]\npackages_dir = /opt/platformio_shared" | sudo tee /etc/platformio/platformio.ini
 
-pio upgrade --no-color
+pio upgrade
 pio --version
 
 pio pkg list --no-color
-pio pkg update --no-color
-pio pkg list --no-color
+pio pkg update
+pio pkg list
 
 ln -s /usr/bin/python3 /usr/bin/python
 
-pio pkg install platformio/framework-arduinoespressif8266 --no-color
-pio pkg install platformio/tool-esptool --no-color
-pio pkg install platformio/tool-esptoolpy --no-color
+pio pkg install platformio/framework-arduinoespressif8266
+pio pkg install platformio/tool-esptool
+pio pkg install platformio/tool-esptoolpy
 
-pio pkg list --no-color
+pio pkg list
