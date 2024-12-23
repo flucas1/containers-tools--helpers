3df905202ca2ff34491ca7e433afc4958000249f
Fernando Lucas <info@fernandolucas.info>
2024-12-23 21:33:10 +0100

    adding some libraries

--- setup-platformio.sh
+++ setup-platformio.sh
@@ -37,6 +37,35 @@ cat >> "${PROJECTDIR}/platformio.ini" << DELIMITER_END_YAML_FILE
 #monitor_speed = 115200
 #upload_speed = 115200
 
+[env]
+lib_deps =
+  RemoteDebug
+  PubSubClient
+  NTPClient
+  ESP Async WebServer
+  EspSoftwareSerial
+  Wire
+  SPI
+  Hash
+  ArduinoJson
+  LinkedList
+  WiFiManager
+  extEEPROM
+  Base64
+  LiquidCrystal_PCF8574
+  Adafruit Unified Sensor
+  Adafruit DHT Unified
+  DHT sensor library
+  RTClib
+  PCF8583
+  PCF8574_ESP
+  ESP8266WiFi
+  INA2xx
+  Adafruit ADS1X15
+  SparkFun BME280
+  BH1750
+  Board Identify
+
 [env:uno]
 platform = atmelavr
 board = uno
