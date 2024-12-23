e0473f663737a0234c6ebe46764726e3ddc90bd5
Fernando Lucas <info@fernandolucas.info>
2024-12-23 19:14:31 +0100

    platformio

--- setup-platformio.sh
+++ setup-platformio.sh
@@ -8,23 +8,22 @@ HELPERSPATH="/helpers"
 #https://github.com/tasmota/docker-tasmota
 
 ${HELPERSPATH}/pip-retry-install.sh platformio
-pio --version
+platformio --version
 
 mkdir -p /opt/platformio_shared
 sudo mkdir -p /etc/platformio
 echo "[platformio]\npackages_dir = /opt/platformio_shared" | sudo tee /etc/platformio/platformio.ini
 
-#pio upgrade
-#pio --version
+#platformio upgrade
+#platformio --version
 
-pio pkg list
-pio pkg update
-pio pkg list
+platformio project init
+platformio pkg list
 
 ln -s /usr/bin/python3 /usr/bin/python
 
-pio pkg install platformio/framework-arduinoespressif8266
-pio pkg install platformio/tool-esptool
-pio pkg install platformio/tool-esptoolpy
+platformio pkg install platformio/framework-arduinoespressif8266
+platformio pkg install platformio/tool-esptool
+platformio pkg install platformio/tool-esptoolpy
 
-pio pkg list
+platformio pkg list
