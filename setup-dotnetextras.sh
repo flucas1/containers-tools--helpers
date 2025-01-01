db8c16d60ccb0032a30e857c25b389c5776a67e4
Fernando Lucas <info@fernandolucas.info>
2025-01-01 01:01:12 +0100

    upd

--- setup-dotnetextras.sh
+++ setup-dotnetextras.sh
@@ -6,18 +6,6 @@ set -x
 MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
 while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
   echo "Retry #$COUNTER"
-  if timeout 900s /opt/dotnet/dotnet workload install wasm-experimental ; then
-    SUCCESS=1
-  else
-    COUNTER=$(( $COUNTER + 1 ))
-    sleep 5s
-  fi
-done
-[ $SUCCESS -eq 1 ]
-
-MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
-while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
-  echo "Retry #$COUNTER"
   if timeout 900s /opt/dotnet/dotnet workload install wasm-tools ; then
     SUCCESS=1
   else
--- wine-dotnetextras.sh
+++ wine-dotnetextras.sh
@@ -8,18 +8,6 @@ WINEATOMIC="/wine-atomic.sh"
 MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
 while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
   echo "Retry #$COUNTER"
-  if timeout 900s $WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" workload install wasm-experimental ; then
-    SUCCESS=1
-  else
-    COUNTER=$(( $COUNTER + 1 ))
-    sleep 5s
-  fi
-done
-[ $SUCCESS -eq 1 ]
-
-MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
-while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
-  echo "Retry #$COUNTER"
   if timeout 900s $WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" workload install wasm-tools ; then
     SUCCESS=1
   else
