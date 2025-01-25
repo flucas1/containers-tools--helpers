ad28bda264a04cdc719e72fb4da83b3d79c6bc69
Fernando Lucas <info@fernandolucas.info>
2025-01-25 21:30:09 +0100

    a2enmod

--- setup-apachecore.sh
+++ setup-apachecore.sh
@@ -15,6 +15,9 @@ ${HELPERSPATH}/apt-retry-install.sh apache2
 /usr/sbin/a2dismod mpm_prefork
 /usr/sbin/a2enmod http2
 /usr/sbin/a2enmod mpm_event
+/usr/sbin/a2enmod headers
+/usr/sbin/a2enmod rewrite
+/usr/sbin/a2enmod setenvif
 
 # Write logs to stderr/stdout
 sed -i 's|^ErrorLog .*$|ErrorLog /dev/stderr|' /etc/apache2/apache2.conf
