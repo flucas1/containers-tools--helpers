b2caa4dc145283b8939efa9e836f2dc9b1e0429f
Fernando Lucas <info@fernandolucas.info>
2025-02-26 20:58:41 +0100

    --allow-change-held-packages

--- apt-force-install.sh
+++ apt-force-install.sh
@@ -8,10 +8,10 @@ dpkg --configure -a
 APTARGUMENTS="-q=1 -y"
 if [ -f /usr/bin/aptitude ] ; then
   APTBINARY="/usr/bin/aptitude"
-  APTARGUMENTS="--without-recommends --allow-new-upgrades --allow-new-installs ${APTARGUMENTS}"
+  APTARGUMENTS="--without-recommends --allow-downgrades --allow-new-upgrades --allow-new-installs ${APTARGUMENTS}"
 else
   APTBINARY="/usr/bin/apt-get"
-  APTARGUMENTS="--no-install-recommends ${APTARGUMENTS}"
+  APTARGUMENTS="--no-install-recommends --allow-downgrades ${APTARGUMENTS}"
 fi
 
 if [ -z "${APTINSTALLVERSION}" ] ; then
@@ -22,5 +22,5 @@ if [ -z "${APTINSTALLVERSION}" ] ; then
   fi
 else
   APTTARGETFULLVERSION=$(apt list --all-versions ${1} 2> /dev/null | grep "^${1}" | awk '{print $2}' | sort -V | awk '$0=="'${APTINSTALLVERSION}'" || $0~"'^${APTINSTALLVERSION}.'" || $0~"'^${APTINSTALLVERSION}-'" { print $0 }' | tail -n 1)
-  "${APTBINARY}" ${APTARGUMENTS} install $1=${APTTARGETFULLVERSION} --allow-downgrades
+  "${APTBINARY}" ${APTARGUMENTS} install $1=${APTTARGETFULLVERSION}
 fi
