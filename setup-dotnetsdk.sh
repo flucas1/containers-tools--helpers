8ea8e5aad2676a7d685e09d8d254e9b7bdb084c0
Fernando Lucas <info@fernandolucas.info>
2025-05-02 23:37:02 +0200

    update

--- setup-dotnetsdk.sh
+++ setup-dotnetsdk.sh
@@ -67,7 +67,7 @@ getversion_dotnetsdk()
   SUCCESS=0
   while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
     echo "Retry #$COUNTER" >&2
-    DOTNETSDKVERSION="$(timeout 900s wget --quiet --no-verbose --retry-connrefused --waitretry=3 --tries=20 https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/releases-index.json -O - | jq -r '.["releases-index"][] | select(."support-phase"=="${SUPPORT}") | ."latest-sdk"' | sort --version-sort --reverse | awk -v n=$LINENUMBER 'NR==n')"
+    DOTNETSDKVERSION="$(timeout 900s wget --quiet --no-verbose --retry-connrefused --waitretry=3 --tries=20 https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/releases-index.json -O - | jq -r '.["releases-index"][] | select(."support-phase"=="'${SUPPORT}'") | ."latest-sdk"' | sort --version-sort --reverse | awk -v n=$LINENUMBER 'NR==n')"
     if [ "${DOTNETSDKVERSION}" != "" ] ; then
       SUCCESS=1
     else
