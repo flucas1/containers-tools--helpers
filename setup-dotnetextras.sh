dbd95ccc67ea64363a2ef210e3c84b2b6c27efdb
Fernando Lucas <info@fernandolucas.info>
2025-05-03 20:20:33 +0200

    update

--- setup-dotnetextras.sh
+++ setup-dotnetextras.sh
@@ -3,12 +3,13 @@
 set -e
 set -x
 
-install_wasmtools()
+install_wasmtoolsversioned()
 {
+  DOTNETSDKVERSION="$1"
   MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
   while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
     echo "Retry #$COUNTER"
-    if timeout 900s /opt/dotnet/dotnet workload install wasm-tools ; then
+    if DOTNET_ROOT="${DOTNETSDKVERSION}" timeout 900s /opt/dotnet/dotnet workload install wasm-tools ; then
       SUCCESS=1
     else
       COUNTER=$(( $COUNTER + 1 ))
@@ -16,6 +17,14 @@ install_wasmtools()
     fi
   done
   [ $SUCCESS -eq 1 ]
+}
+
+install_wasmtools()
+{
+  DOTNETSDKS="$(/opt/dotnet/dotnet --list-sdks | awk '{first=$1; rest=substr($0, length($1)+2); print substr(rest, 2, length(rest)-2) "/" first "/Sdks"}')"
+  for DOTNETSDKVERSION in $DOTNETSDKS ; do
+    install_wasmtoolsversioned $DOTNETSDKVERSION
+  done
 }
 
 install_avaloniatemplates()
--- wine-dotnetextras.sh
+++ wine-dotnetextras.sh
@@ -5,12 +5,13 @@ set -x
 
 WINEATOMIC="/wine-atomic.sh"
 
-install_wasmtools()
+install_wasmtoolsversioned()
 {
+  DOTNETSDKVERSION="$1"
   MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
   while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
     echo "Retry #$COUNTER"
-    if timeout 900s $WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" workload install wasm-tools ; then
+    if DOTNET_ROOT="${DOTNETSDKVERSION}" timeout 900s $WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" workload install wasm-tools ; then
       SUCCESS=1
     else
       COUNTER=$(( $COUNTER + 1 ))
@@ -19,6 +20,15 @@ install_wasmtools()
   done
   [ $SUCCESS -eq 1 ]
 }
+
+install_wasmtools()
+{
+  DOTNETSDKS="$($WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" --list-sdks | awk '{first=$1; rest=substr($0, length($1)+2); print substr(rest, 2, length(rest)-2) "\\" first "\\Sdks"}')"
+  for DOTNETSDKVERSION in $DOTNETSDKS ; do
+    install_wasmtoolsversioned $DOTNETSDKVERSION
+  done
+}
+
 install_avaloniatemplates()
 {
   MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
