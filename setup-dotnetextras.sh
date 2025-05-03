23224c5fcd44281fb775fa490e7b75d03a3d141a
Fernando Lucas <info@fernandolucas.info>
2025-05-03 10:45:53 +0200

    fist fix

--- setup-dotnetextras.sh
+++ setup-dotnetextras.sh
@@ -3,28 +3,37 @@
 set -e
 set -x
 
-MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
-while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
-  echo "Retry #$COUNTER"
-  if timeout 900s /opt/dotnet/dotnet workload install wasm-tools ; then
-    SUCCESS=1
-  else
-    COUNTER=$(( $COUNTER + 1 ))
-    sleep 5s
-  fi
-done
-[ $SUCCESS -eq 1 ]
+install_wasmtools()
+{
+  MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
+  while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
+    echo "Retry #$COUNTER"
+    if timeout 900s /opt/dotnet/dotnet workload install wasm-tools ; then
+      SUCCESS=1
+    else
+      COUNTER=$(( $COUNTER + 1 ))
+      sleep 5s
+    fi
+  done
+  [ $SUCCESS -eq 1 ]
+}
 
-MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
-while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
-  echo "Retry #$COUNTER"
-  if timeout 900s /opt/dotnet/dotnet new install avalonia.templates ; then
-    SUCCESS=1
-  else
-    COUNTER=$(( $COUNTER + 1 ))
-    sleep 5s
-  fi
-done
-[ $SUCCESS -eq 1 ]
+install_avaloniatemplates()
+{
+  MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
+  while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
+    echo "Retry #$COUNTER"
+    if timeout 900s /opt/dotnet/dotnet new install avalonia.templates ; then
+      SUCCESS=1
+    else
+      COUNTER=$(( $COUNTER + 1 ))
+      sleep 5s
+    fi
+  done
+  [ $SUCCESS -eq 1 ]
+}
 
-#$WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" tool install --global dotnet-outdated-tool
+#/opt/dotnet/dotnet tool install --global dotnet-outdated-tool
+
+install_wasmtools()
+install_avaloniatemplates()
--- wine-dotnetextras.sh
+++ wine-dotnetextras.sh
@@ -5,26 +5,38 @@ set -x
 
 WINEATOMIC="/wine-atomic.sh"
 
-MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
-while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
-  echo "Retry #$COUNTER"
-  if timeout 900s $WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" workload install wasm-tools ; then
-    SUCCESS=1
-  else
-    COUNTER=$(( $COUNTER + 1 ))
-    sleep 5s
-  fi
-done
-[ $SUCCESS -eq 1 ]
+install_wasmtools()
+{
+  MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
+  while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
+    echo "Retry #$COUNTER"
+    if timeout 900s $WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" workload install wasm-tools ; then
+      SUCCESS=1
+    else
+      COUNTER=$(( $COUNTER + 1 ))
+      sleep 5s
+    fi
+  done
+  [ $SUCCESS -eq 1 ]
+}
 
-MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
-while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
-  echo "Retry #$COUNTER"
-  if timeout 900s $WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" new install avalonia.templates ; then
-    SUCCESS=1
-  else
-    COUNTER=$(( $COUNTER + 1 ))
-    sleep 5s
-  fi
-done
-[ $SUCCESS -eq 1 ]
+install_avaloniatemplates()
+{
+  MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
+  while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
+    echo "Retry #$COUNTER"
+    if timeout 900s $WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" new install avalonia.templates ; then
+      SUCCESS=1
+    else
+      COUNTER=$(( $COUNTER + 1 ))
+      sleep 5s
+    fi
+  done
+  [ $SUCCESS -eq 1 ]
+}
+
+#$WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" tool install --global dotnet-outdated-tool
+
+install_wasmtools()
+install_avaloniatemplates()
+
