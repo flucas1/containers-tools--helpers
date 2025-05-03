9a633a18fe0593b2fc1cbd470d1df4852f83bea4
Fernando Lucas <info@fernandolucas.info>
2025-05-03 23:18:56 +0200

    elif

--- wine-dotnetsdk.sh
+++ wine-dotnetsdk.sh
@@ -82,10 +82,10 @@ if [ "${ARCHITECTURE}" = "amd64" ] ; then PARTARCH="x64" ; else if [ "${ARCHITEC
 if [ "${DESIREDVERSION}" == "preview" ] ; then
   DOTNETSDKVERSION="$(getversion_dotnetsdk ${PARTARCH} preview 1)"
   install_dotnetsdk "${PARTARCH}" "${DOTNETSDKVERSION}"
-else if [ "${DESIREDVERSION}" == "newest" ] ; then
+elif [ "${DESIREDVERSION}" == "newest" ] ; then
   DOTNETSDKVERSION="$(getversion_dotnetsdk ${PARTARCH} active 1)"
   install_dotnetsdk "${PARTARCH}" "${DOTNETSDKVERSION}"
-else if [ "${DESIREDVERSION}" == "previous" ] ; then
+elif [ "${DESIREDVERSION}" == "previous" ] ; then
   DOTNETSDKVERSION="$(getversion_dotnetsdk ${PARTARCH} active 2)"
   install_dotnetsdk "${PARTARCH}" "${DOTNETSDKVERSION}"
 else
