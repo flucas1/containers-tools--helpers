a42ac9aa71f8e1b6a63c404f07ad8b6c0b144821
Fernando Lucas <info@fernandolucas.info>
2025-05-05 00:34:31 +0200

    targetFramework

--- wine-dotnetextras.sh
+++ wine-dotnetextras.sh
@@ -24,10 +24,10 @@ install_wasmtoolsmultiple()
 {
   DOTNETSDKS="$($WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" --list-sdks | awk '{print $1}')"
   for DOTNETSDKVERSION in $DOTNETSDKS ; do
-    $WINEATOMIC del /F /Q ".\\global.json"
+    $WINEATOMIC "C:\\Windows\\System32\\cmd.exe" /c del /F /Q ".\\global.json"
     $WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" new globaljson --sdk-version $DOTNETSDKVERSION --output ".\\"
     install_wasmtoolscurrent $DOTNETSDKVERSION
-    $WINEATOMIC del /F /Q ".\\global.json"
+    $WINEATOMIC "C:\\Windows\\System32\\cmd.exe" /c del /F /Q ".\\global.json"
   done
 }
 
