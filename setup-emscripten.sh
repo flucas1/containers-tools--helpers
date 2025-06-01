90802e4df309424689e0feffb2a479d401260815
Fernando Lucas <info@fernandolucas.info>
2025-06-01 22:05:34 +0200

    wget

--- setup-emscripten.sh
+++ setup-emscripten.sh
@@ -9,7 +9,7 @@ mkdir -p /opt/emsdk
 git clone https://github.com/emscripten-core/emsdk.git /opt/emsdk
 git -C /opt/emsdk pull
 
-EMSDKVERSION="$(wget -qO- https://raw.githubusercontent.com/dotnet/runtimelab/refs/heads/feature/NativeAOT-LLVM/eng/pipelines/runtimelab/install-emscripten.ps1 | grep '^./emsdk install' | awk '{print $3}')"
+EMSDKVERSION="$(timeout --kill-after=5s 900s wget --quiet --no-verbose --retry-connrefused --waitretry=3 --tries=20 -O - https://raw.githubusercontent.com/dotnet/runtimelab/refs/heads/feature/NativeAOT-LLVM/eng/pipelines/runtimelab/install-emscripten.ps1 | grep '^./emsdk install' | awk '{print $3}')"
 [ "$EMSDKVERSION" != "" ]
 
 /opt/emsdk/emsdk install "$EMSDKVERSION"
