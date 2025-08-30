af04763dc26df3735a4441ed57952ec7720d69fe
Fernando Lucas <info@fernandolucas.info>
2025-08-30 17:41:11 +0200

    /usr/bin/

--- apt-force-install.sh
+++ apt-force-install.sh
@@ -6,13 +6,13 @@ set -x
 dpkg --configure -a
 
 APTARGUMENTS="-q=1 -y"
-#if [ -f /usr/bin/aptitude ] ; then
-#  APTBINARY="/usr/bin/aptitude"
-#  APTARGUMENTS="--without-recommends --allow-new-upgrades --allow-new-installs ${APTARGUMENTS}"
-#else
+if [ -f /usr/bin/aptitude ] ; then
+  APTBINARY="/usr/bin/aptitude"
+  APTARGUMENTS="--without-recommends --allow-new-upgrades --allow-new-installs ${APTARGUMENTS}"
+else
   APTBINARY="/usr/bin/apt-get"
   APTARGUMENTS="--no-install-recommends --allow-downgrades ${APTARGUMENTS}"
-#fi
+fi
 
 if [ -z "${APTINSTALLVERSION}" ] ; then
   if [ -z "${APTINSTALLRELEASE}" ] ; then
@@ -21,6 +21,7 @@ if [ -z "${APTINSTALLVERSION}" ] ; then
     "${APTBINARY}" ${APTARGUMENTS} install $1/$APTINSTALLRELEASE
   fi
 else
-  APTTARGETFULLVERSION=$(apt list --all-versions ${1} 2> /dev/null | grep "^${1}" | awk '{print $2}' | sort -V | awk '$0=="'${APTINSTALLVERSION}'" || $0~"'^${APTINSTALLVERSION}.'" || $0~"'^${APTINSTALLVERSION}-'" { print $0 }' | tail -n 1)
+  APTTARGETFULLVERSION=$(/usr/bin/apt list --all-versions ${1} 2> /dev/null | grep "^${1}" | awk '{print $2}' | sort -V | awk '$0=="'${APTINSTALLVERSION}'" || $0~"'^${APTINSTALLVERSION}.'" || $0~"'^${APTINSTALLVERSION}-'" { print $0 }' | tail -n 1)
   "${APTBINARY}" ${APTARGUMENTS} install $1=${APTTARGETFULLVERSION}
 fi
+/usr/bin/dpkg -s $1 >/dev/null 2>&1
--- apt-try-remove.sh
+++ apt-try-remove.sh
@@ -3,4 +3,4 @@
 set -e
 set -x
 
-if dpkg -s $1 >/dev/null 2>&1 ; then apt-get remove -y $1 ; fi
+if /usr/bin/dpkg -s $1 >/dev/null 2>&1 ; then /usr/bin/apt-get remove -y $1 ; fi
