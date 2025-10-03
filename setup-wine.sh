0292ffdb61301e0f494b8de7b4e8dd89cd5965be
Fernando Lucas <info@fernandolucas.info>
2025-10-03 02:09:48 +0200

    udev acl

--- setup-wine.sh
+++ setup-wine.sh
@@ -38,8 +38,10 @@ ${HELPERSPATH}/apt-retry-install.sh wget
 ${HELPERSPATH}/apt-retry-install.sh bluez
 ${HELPERSPATH}/apt-retry-install.sh winbind
 ${HELPERSPATH}/apt-retry-install.sh fontconfig
+${HELPERSPATH}/apt-retry-install.sh udev
+${HELPERSPATH}/apt-retry-install.sh acl
 
-REFERENCEPACKAGES="libavahi-client3 libjbig0 libmount1 libudev1 udev libcurl3t64-gnutls libgd3 libgphoto2-6t64 libsane1 libsane-common acl libpci3 pci.ids libsnmp40t64 libsnmp-base"
+REFERENCEPACKAGES="libavahi-client3 libjbig0 libmount1 libudev1 libcurl3t64-gnutls libgd3 libgphoto2-6t64 libsane1 libsane-common libpci3 pci.ids libsnmp40t64 libsnmp-base"
 FINALPACKAGES=""
 for TESTPACKAGE in $(echo "${REFERENCEPACKAGES}"); do
   FINALPACKAGES="${FINALPACKAGES} ${TESTPACKAGE}"
