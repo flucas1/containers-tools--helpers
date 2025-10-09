7d7f024a986fc713ee42405e3d2bbe9ec3b5cb75
Fernando Lucas <info@fernandolucas.info>
2025-10-09 10:43:00 +0200

    update

--- apt-upgrade.sh
+++ apt-upgrade.sh
@@ -6,5 +6,31 @@ set -x
 HELPERSPATH="/helpers"
 
 ${HELPERSPATH}/apt-retry-install.sh aptitude
-aptitude safe-upgrade -y
-aptitude full-upgrade --without-recommends --allow-new-upgrades --allow-new-installs
+
+MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
+while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
+  echo "Retry #$COUNTER"
+  if timeout --kill-after=5s 900s aptitude safe-upgrade -y ; then
+    SUCCESS=1
+  else
+    COUNTER=$(( $COUNTER + 1 ))
+    # extra delay to allow apt-cacher-ng to rebuild indexes
+    # with 5s delay, the whole loop lasts less than 3 min
+    sleep 30s
+  fi
+done
+[ $SUCCESS -eq 1 ]
+
+MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
+while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
+  echo "Retry #$COUNTER"
+  if timeout --kill-after=5s 900s aptitude full-upgrade --without-recommends --allow-new-upgrades --allow-new-installs ; then
+    SUCCESS=1
+  else
+    COUNTER=$(( $COUNTER + 1 ))
+    # extra delay to allow apt-cacher-ng to rebuild indexes
+    # with 5s delay, the whole loop lasts less than 3 min
+    sleep 30s
+  fi
+done
+[ $SUCCESS -eq 1 ]
