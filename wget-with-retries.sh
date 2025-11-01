8dc8ca496a188ca36c9b6ec60dfaaf119f733173
Fernando Lucas <info@fernandolucas.info>
2025-11-01 10:59:44 +0100

    FIXING

--- wget-with-retries.sh
+++ wget-with-retries.sh
@@ -6,4 +6,14 @@ set -x
 DOWNLOADURL="$1"
 DOWNLOADFILE="$2"
 
-timeout --kill-after=5s 900s wget -4 --quiet --no-verbose --retry-connrefused --waitretry=3 --tries=20 "$DOWNLOADURL" -O "$DOWNLOADFILE"
+MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
+while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
+  echo "Retry #$COUNTER"
+  if timeout --kill-after=5s 900s wget -4 --quiet --no-verbose --retry-connrefused --waitretry=1 --tries=10 "$DOWNLOADURL" -O "$DOWNLOADFILE" ; then
+    SUCCESS=1
+  else
+    COUNTER=$(( $COUNTER + 1 ))
+    sleep 5s
+  fi
+done
+[ $SUCCESS -eq 1 ]
