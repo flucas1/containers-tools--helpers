d9a427df1bed316a5ed9ff4567912a866122e21d
Fernando Lucas <info@fernandolucas.info>
2026-03-01 21:15:43 +0100

    upd

--- wine-wix.sh
+++ wine-wix.sh
@@ -21,17 +21,17 @@ install_wix()
   
   $WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" tool list --global
   
-  MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
-  while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
-    echo "Retry #$COUNTER" >&2
-    if timeout --kill-after=5s 900s /usr/bin/ipv4 $WINEATOMIC "C:\\users\\$(whoami)\\.dotnet\\tools\\wix.exe" extension add WixToolset.Msix ; then
-      SUCCESS=1
-    else
-      COUNTER=$(( $COUNTER + 1 ))
-      sleep 5s
-    fi
-  done
-  [ $SUCCESS -eq 1 ]
+#  MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
+#  while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
+#    echo "Retry #$COUNTER" >&2
+#    if timeout --kill-after=5s 900s /usr/bin/ipv4 $WINEATOMIC "C:\\users\\$(whoami)\\.dotnet\\tools\\wix.exe" extension add WixToolset.Msix ; then
+#      SUCCESS=1
+#    else
+#      COUNTER=$(( $COUNTER + 1 ))
+#      sleep 5s
+#    fi
+#  done
+#  [ $SUCCESS -eq 1 ]
 }
 
 install_wix
