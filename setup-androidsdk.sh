f0c769ead32244c318e7675564ac4979354b2867
Fernando Lucas <info@fernandolucas.info>
2026-03-28 14:52:36 +0100

    update

--- setup-androidsdk.sh
+++ setup-androidsdk.sh
@@ -7,10 +7,10 @@ HELPERSPATH="/helpers"
 
 /helpers/apt-retry-install.sh google-android-cmdline-tools-latest-installer
 /helpers/apt-retry-install.sh google-android-platform-tools-installer
-/helpers/apt-retry-install.sh google-android-build-tools-33.0.3-installer
+/helpers/apt-retry-install.sh google-android-build-tools-35.0.1-installer
 ln -s $(ls -d /usr/lib/android-sdk/cmdline-tools/*/ | head -n1) /usr/lib/android-sdk/cmdline-tools/latest
 yes | /usr/lib/android-sdk/cmdline-tools/latest/bin/sdkmanager --licenses
 /usr/lib/android-sdk/cmdline-tools/latest/bin/sdkmanager --list
-/usr/lib/android-sdk/cmdline-tools/latest/bin/sdkmanager "platforms;android-33"
+/usr/lib/android-sdk/cmdline-tools/latest/bin/sdkmanager "platforms;android-35"
 /usr/lib/android-sdk/cmdline-tools/latest/bin/sdkmanager "platform-tools"
 /usr/lib/android-sdk/cmdline-tools/latest/bin/sdkmanager --list
