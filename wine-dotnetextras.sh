8b27e7d0d84c7b1af541e28deecad29275dfb990
Fernando Lucas <info@fernandolucas.info>
2026-03-28 12:08:41 +0100

    update

--- wine-dotnetextras.sh
+++ wine-dotnetextras.sh
@@ -31,6 +31,32 @@ install_wasmtoolsmultiple()
   done
 }
 
+install_androidcurrent()
+{
+  MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
+  while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
+    echo "Retry #$COUNTER" >&2
+    if timeout --kill-after=5s 900s /usr/bin/ipv4 $WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" workload install android ; then
+      SUCCESS=1
+    else
+      COUNTER=$(( $COUNTER + 1 ))
+      sleep 5s
+    fi
+  done
+  [ $SUCCESS -eq 1 ]
+}
+
+install_androidmultiple()
+{
+  DOTNETSDKS="$($WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" --list-sdks | awk '{print $1}')"
+  for DOTNETSDKVERSION in $DOTNETSDKS ; do
+    rm -f ./global.json
+    $WINEATOMIC "C:\\Program Files\\dotnet\\dotnet.exe" new globaljson --sdk-version $DOTNETSDKVERSION --output ".\\"
+    install_androidcurrent $DOTNETSDKVERSION
+    rm -f ./global.json
+  done
+}
+
 install_avaloniatemplates()
 {
   MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
@@ -62,6 +88,8 @@ install_dotnetoutdatedtool()
 }
 
 install_wasmtoolsmultiple
+
+install_androidmultiple
 
 #install_avaloniatemplates
 
