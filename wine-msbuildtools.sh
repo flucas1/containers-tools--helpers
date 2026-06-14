0bcf91700e5ca4c263be08c2e9e256883bdcb8e6
Fernando Lucas <info@fernandolucas.info>
2026-06-14 15:20:40 +0200

    update

--- wine-msbuildtools.sh
+++ wine-msbuildtools.sh
@@ -11,23 +11,27 @@ if [ "${DIRECTINSTALL}" = "yes" ] ; then
   VISUALSTUDIOVERSION="18"
   VISUALSTUDIOCHANNEL="Insiders"
 
-  /helpers/wget-with-retries.sh "https://aka.ms/vs/${VISUALSTUDIOVERSION}/${VISUALSTUDIOCHANNEL}/vs_BuildTools.exe" ./vs_buildtools.exe
-  /helpers/wget-with-retries.sh "https://aka.ms/vs/${VISUALSTUDIOVERSION}/${VISUALSTUDIOCHANNEL}/installer" ./vs_installer.zip
-  mkdir -p "$WINEPREFIX/drive_c/Program Files (x86)/Microsoft Visual Studio/Installer"
-  mkdir -p "/tmp/vs_installer/"
-  unzip ./vs_installer.zip -d "/tmp/vs_installer/"
-  cp -r /tmp/vs_installer/Contents/. "$WINEPREFIX/drive_c/Program Files (x86)/Microsoft Visual Studio/Installer/"
-  rm -rf "/tmp/vs_installer/"
-  rm -f ./vs_installer.zip
-  $WINEATOMIC ./vs_buildtools.exe --quiet --wait --layout C:\\VSLayout --lang en-US --add Microsoft.VisualStudio.Workload.VCTools
-  rm -f ./vs_buildtools.exe
-
   WINVER="$(${HELPERSPATH}/wine-getver.sh)"
   ${HELPERSPATH}/wine-setver.sh win11
-  $WINEATOMIC C:\\VSLayout\\vs_setup.exe --quiet --wait --noUpdateInstaller --noWeb --norestart || true
-  ${HELPERSPATH}/wine-setver.sh "${WINVER}"
 
+  /helpers/wget-with-retries.sh "https://aka.ms/vs/${VISUALSTUDIOVERSION}/${VISUALSTUDIOCHANNEL}/vs_BuildTools.exe" ./vs_buildtools.exe
+  
+  /helpers/wget-with-retries.sh "https://aka.ms/vs/${VISUALSTUDIOVERSION}/${VISUALSTUDIOCHANNEL}/installer" ./vs_installer.zip
+  mkdir -p "/tmp/vs_installer/"
+  unzip ./vs_installer.zip -d "/tmp/vs_installer/"
+  rm -f ./vs_installer.zip
+  mkdir -p "$WINEPREFIX/drive_c/Program Files (x86)/Microsoft Visual Studio/Installer/"
+  cp -r /tmp/vs_installer/Contents/. "$WINEPREFIX/drive_c/Program Files (x86)/Microsoft Visual Studio/Installer/"
+  rm -rf "/tmp/vs_installer/"
+  
+  $WINEATOMIC ./vs_buildtools.exe --quiet --wait --layout C:\\VSLayout --lang en-US --add Microsoft.VisualStudio.Workload.VCTools
+
+  $WINEATOMIC C:\\VSLayout\\vs_setup.exe --quiet --wait --noUpdateInstaller --noWeb --norestart || true
+  
   $WINEATOMIC cmd /c rmdir /s /q C:\\VSLayout
+  rm -f ./vs_buildtools.exe
+
+  ${HELPERSPATH}/wine-setver.sh "${WINVER}"
 else
   winetricks vstools2019
 fi
