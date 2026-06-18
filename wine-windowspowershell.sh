871be8b609d9b52d818d4212d736683de54a8a93
Fernando Lucas <info@fernandolucas.info>
2026-06-18 07:31:33 +0200

    update

--- wine-windowspowershell.sh
+++ wine-windowspowershell.sh
@@ -6,10 +6,12 @@ set -x
 WINEATOMIC="/wine-atomic.sh"
 DIRECTINSTALL="$1"
 
-winetricks --optout --force -q win2k3 > /dev/null
-$WINEATOMIC /home/wineuser/.cache/winetricks/powershell10/WindowsServer2003.WindowsXP-KB926139-v2-x64-ENU.exe /quiet /passive /norestart
-winetricks --optout --force -q winver= > /dev/null
+WINVER="$(${HELPERSPATH}/wine-getver.sh)"
 
-winetricks --optout --force -q win2k3 > /dev/null
+${HELPERSPATH}/wine-setver.sh win2k3
+$WINEATOMIC /home/wineuser/.cache/winetricks/powershell10/WindowsServer2003.WindowsXP-KB926139-v2-x64-ENU.exe /quiet /passive /norestart
+
+${HELPERSPATH}/wine-setver.sh win2k3
 $WINEATOMIC /home/wineuser/.cache/winetricks/powershell20/WindowsServer2003-KB968930-x64-ENG.exe /quiet /passive /norestart
-winetricks --optout --force -q winver= > /dev/null
+
+${HELPERSPATH}/wine-setver.sh "${WINVER}"
