77896261e3887d83198311cfb4cb2e25138aae4e
Fernando Lucas <info@fernandolucas.info>
2026-06-21 11:46:44 +0200

    inirial

---
+++ setup-ltp.sh
@@ -0,0 +1,8 @@
+mkdir -p /opt/ltp
+
+/helpers/pip-retry-install.sh language-tool-python
+python3 -c "import language_tool_python; tool=language_tool_python.LanguageTool('en'); tool.close()"
+
+#/helpers/wget-with-retries.sh https://www.languagetool.org/download/LanguageTool-stable.zip /opt/ltp/ltp-stable.zip
+#unzip /opt/ltp/ltp-stable.zip -d /opt/ltp
+#rm -f /opt/ltp/ltp-stable.zip
