745de75a4df46c907e8de6d72a7fb56f88152eeb
Fernando Lucas <info@fernandolucas.info>
2026-06-21 12:03:59 +0200

    retries

--- setup-ltp.sh
+++ setup-ltp.sh
@@ -9,7 +9,18 @@ HELPERSPATH="/helpers"
 mkdir -p "${LTP_PATH}"
 
 ${HELPERSPATH}/pip-retry-install.sh language-tool-python
-python3 -c "import language_tool_python; tool=language_tool_python.LanguageTool('en'); tool.close()"
+
+MAXRETRIES=30 ; COUNTER=0 ; SUCCESS=0
+while [ $SUCCESS -eq 0 ] && [ $COUNTER -lt $MAXRETRIES ] ; do
+  echo "Retry #$COUNTER" >&2
+  if python3 -c "import language_tool_python; tool=language_tool_python.LanguageTool('en'); tool.close()" ; then
+    SUCCESS=1
+  else
+    COUNTER=$(( $COUNTER + 1 ))
+    sleep 5s
+  fi
+done
+[ $SUCCESS -eq 1 ]
 
 #${HELPERSPATH}/wget-with-retries.sh https://www.languagetool.org/download/LanguageTool-stable.zip "${LTP_PATH}"/ltp-stable.zip
 #unzip "${LTP_PATH}"/ltp-stable.zip -d /opt/ltp
