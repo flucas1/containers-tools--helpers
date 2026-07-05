2e595e0cc34ea796c60cc652952c16a163016270
Fernando Lucas <info@fernandolucas.info>
2026-07-05 15:56:22 +0200

    update

--- setup-apachephp.sh
+++ setup-apachephp.sh
@@ -5,24 +5,18 @@ set -x
 
 HELPERSPATH="/helpers"
 
-#${HELPERSPATH}/apt-retry-install.sh php-fpm
-#/usr/sbin/a2enmod proxy_fcgi
-#/usr/sbin/a2enmod setenvif
-#PHPCONF=$(for item in $(ls /etc/apache2/conf-available/php*-fpm.conf) ; do basename $item ; done | sort --version-sort | tail -n 1)
-#[ "$PHPCONF" != "" ]
-#/usr/sbin/a2enconf $(echo $PHPCONF | rev | cut -c6- | rev)
-
-${HELPERSPATH}/apt-retry-install.sh php
-/usr/bin/php -v
-PHPMODULE=$(for item in $(ls /etc/apache2/mods-available/php*.load) ; do basename $item ; done | sort --version-sort | tail -n 1)
+${HELPERSPATH}/apt-retry-install.sh php-fpm
+/usr/sbin/a2enmod proxy_fcgi
+/usr/sbin/a2enmod setenvif
+PHPMODULE=$(for item in $(ls /etc/apache2/conf-available/php*-fpm.conf) ; do basename $item ; done | sort --version-sort | tail -n 1)
 [ "$PHPMODULE" != "" ]
-/usr/sbin/a2enmod $(echo $PHPMODULE | rev | cut -c6- | rev)
+/usr/sbin/a2enconf $(echo $PHPMODULE | awk '{print substr($0,1,length($0)-5)}')
 
 ${HELPERSPATH}/apt-retry-install.sh php-sqlite3
 ${HELPERSPATH}/apt-retry-install.sh php-gd
 /usr/bin/php -m
 
-PHPVERSION=$(echo $PHPMODULE | rev | cut -c6- | rev | cut -c4-)
+PHPVERSION=$(echo $PHPMODULE | awk '{print substr($0,1,length($0)-9)}' | cut -c4-)
 if [ "$PHPVERSION" = "8.4" ] ; then
-  echo "zend.max_allowed_stack_size = -1" > /etc/php/$PHPVERSION/apache2/conf.d/99-stack.ini
+  echo "zend.max_allowed_stack_size = -1" > /etc/php/$PHPVERSION/fpm/conf.d/99-stack.ini
 fi
